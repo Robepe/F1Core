e6d212952c5e321cff4b6dd2c729c1a91a39a0a1
Robepe <ghosthamer@gmail.com>
2023-11-05 17:42:48 +0100

    Add module @ngx-translate

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// Crea una función para cargar las traducciones desde archivos JSON
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    // ... tus componentes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule, // Agrega HttpClientModule para el uso de HttpLoaderFactory
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    // ... tu componente raíz
+  ],
 })
 export class AppModule { }
+
