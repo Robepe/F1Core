c6579eb694b73a22309cf8161731d1bb69459804
Robepe <ghosthamer@gmail.com>
2023-12-21 18:25:22 +0100

    feat: Add methods for GET, POST, PUT & DELETE

--- client/src/app/services/constructors/constructor-service.service.ts
+++ client/src/app/services/constructors/constructor-service.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ConstructorServiceService {
+export class DriverService {
+  private apiUrl = 'http://localhost:4201/api/constructors';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getConstructors(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addConstructor(constructor: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, constructor);
+  }
+
+  updateConstructor(constructorId: number, updatedDriver: any): Observable<any> {
+    const url = `${this.apiUrl}/${constructorId}`;
+    return this.http.put<any>(url, updatedDriver);
+  }
+
+  deleteConstructor(constructorId: number): Observable<any> {
+    const url = `${this.apiUrl}/${constructorId}`;
+    return this.http.delete<any>(url);
+  }
 }
--- client/src/app/services/results/results-service.service.ts
+++ client/src/app/services/results/results-service.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ResultsServiceService {
+export class DriverService {
+  private apiUrl = 'http://localhost:4201/api/results';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getResults(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addResult(result: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, result);
+  }
+
+  updateResult(resultId: number, updatedDriver: any): Observable<any> {
+    const url = `${this.apiUrl}/${resultId}`;
+    return this.http.put<any>(url, updatedDriver);
+  }
+
+  deleteResult(resultId: number): Observable<any> {
+    const url = `${this.apiUrl}/${resultId}`;
+    return this.http.delete<any>(url);
+  }
 }
