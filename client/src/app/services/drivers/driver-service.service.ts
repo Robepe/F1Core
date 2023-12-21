94a6347e04a6eace3b642b727213d662063c5695
Robepe <ghosthamer@gmail.com>
2023-12-21 15:31:48 +0100

    feat: Add Services for 'drivers', 'results', & 'constructors'

---
+++ client/src/app/services/drivers/driver-service.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DriverService {
+  private apiUrl = 'http://localhost:4201/api/drivers';
+
+  constructor(private http: HttpClient) {}
+
+  getDrivers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addDriver(driver: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, driver);
+  }
+
+  updateDriver(driverId: number, updatedDriver: any): Observable<any> {
+    const url = `${this.apiUrl}/${driverId}`;
+    return this.http.put<any>(url, updatedDriver);
+  }
+
+  deleteDriver(driverId: number): Observable<any> {
+    const url = `${this.apiUrl}/${driverId}`;
+    return this.http.delete<any>(url);
+  }
+}
+
+
