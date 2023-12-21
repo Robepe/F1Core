bb038bcc1f2372db2cb183ac8bbf93402fc52d47
Robepe <ghosthamer@gmail.com>
2023-12-21 15:35:45 +0100

    feat: Add Driver Tables

--- client/src/app/components/pilots/pilots.component.ts
+++ client/src/app/components/pilots/pilots.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DriverService } from 'src/app/services/drivers/driver-service.service';
 
 @Component({
-  selector: 'app-pilots',
-  templateUrl: './pilots.component.html',
-  styleUrls: ['./pilots.component.css']
+    selector: 'app-pilots',
+    templateUrl: './pilots.component.html',
+    styleUrls: ['./pilots.component.css']
 })
-export class PilotsComponent {
+export class PilotsComponent implements OnInit {
+    drivers: any[] = [];
 
+    constructor(private driverService: DriverService) { };
+
+    ngOnInit(): void {
+        this.getDrivers();
+    }
+
+    getDrivers(): void {
+        this.driverService.getDrivers().subscribe((data) => {
+            this.drivers = data;
+        });
+    }
 }
