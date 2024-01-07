b3bcacba7b16ea8662383935b9c13b01388afeee
Robepe <ghosthamer@gmail.com>
2024-01-07 18:55:12 +0100

    session storage scope - dashboard

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  username: string = '';
 
+  constructor(private storageService: StorageService) { }
+
+  ngOnInit(): void {
+    const user = this.storageService.getUser();
+    this.username = user.username;
+  }
 }
